78062834d36023847974b4bda52018ad0a312f2d
body-mode <battmartos@gmail.com>
2014-04-28 23:57:53 -0700

    fix columns
    fix columns to 4-col format

---
+++ fix_columns.py
@@ -0,0 +1,12 @@
+import os
+
+def convert_cols():
+	for fn in os.listdir('.'):
+		f = open(fn, 'r')
+		r = f.readlines()
+		f = open(fn, 'w')
+		for i in r:
+			s = i.split(' ')[3:7]
+			j = ' '.join(s)
+			f.write(j)
+		f.close()
